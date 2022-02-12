25e39ad810881a1d4a6c3a0e79ba51e6135c137b
Muriele Gudim <murielegudim@gmail.com>
2022-02-12 18:55:32 -0300

    implementations

---
+++ src/entities/Company.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Company extends TaxPayer {
+
+	private int numberOfEmpployees;
+	
+	public Company() {
+	}
+	
+	public Company(String name, Double anualIncome, int numberOfEmpployees) {
+		super(name, anualIncome);
+		this.numberOfEmpployees = numberOfEmpployees;
+	}
+	
+    public int getNumberOfEmpployees() {
+		return numberOfEmpployees;
+	}
+
+	public void setNumberOfEmpployees(int numberOfEmpployees) {
+		this.numberOfEmpployees = numberOfEmpployees;
+	}
+
+	@Override
+	public Double tax() {
+		if (numberOfEmpployees > 10) {
+			return getAnualIncome() * 0.14;
+		}
+		else {
+			return getAnualIncome() * 0.16;
+		}
+	}
+
+}
---
+++ src/entities/Individual.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Individual extends TaxPayer {
+	
+	private Double healthExpenditures;
+	
+	public Individual () {
+	}
+	
+	public Individual(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+	
+    public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHealthExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	@Override
+	public Double tax() {
+		double basicTax;
+		if (getAnualIncome() < 20000.00) {
+			basicTax = getAnualIncome() * 0.15;
+		}
+		else {
+			basicTax = getAnualIncome() * 0.25;
+		}
+		basicTax = basicTax - getHealthExpenditures() * 0.5;
+		if (basicTax < 0.0) {
+			return 0.0;
+		}
+		else {
+			return basicTax;
+		}
+	}
+
+}
