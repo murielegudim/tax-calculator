525c25fe0fcd27c6a83cf39e5388c4758177038a
Muriele Gudim <murielegudim@gmail.com>
2022-02-13 17:10:39 -0300

    correcttions

--- src/entities/Individual.java
+++ src/entities/Individual.java
@@ -6,16 +6,13 @@ public class Individual extends TaxPayer {
 	
 	public Individual () {
 	}
-	
 	public Individual(String name, Double anualIncome, Double healthExpenditures) {
 		super(name, anualIncome);
 		this.healthExpenditures = healthExpenditures;
 	}
-	
-    public Double getHealthExpenditures() {
+	public Double getHealthExpenditures() {
 		return healthExpenditures;
 	}
-
 	public void setHealthExpenditures(Double healthExpenditures) {
 		this.healthExpenditures = healthExpenditures;
 	}
